48f95acc214cda28d5fced1ff524880d4f96358d
Yusuf <yusufrehmanit@gmail.com>
2026-01-01 09:48:03 -0800

    setup

---
+++ main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = var.enable_nat_gateway
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+// Bastion Host  -- Bouncer
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.bastion_instance_type
+
+  tags = {
+    Name = "Bastion Host"
+  }
+}
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion_sg"
+  description = "Security group for Bastion Host"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "azs" {
+    description = "A list of availability zones in which to create the VPC subnets."
+    type        = list(string)
+    default     = ["us-west-1a", "us-west-1b"]
+}
+variable "private_subnets" {
+    description = "A list of CIDR blocks for the private subnets."
+    type        = list(string)
+    default     = ["10.0.1.0/24"]
+}
+variable "public_subnets" {
+    description = "A list of CIDR blocks for the public subnets."
+    type        = list(string)
+    default     = ["10.0.101.0/24"]
+}
+variable "vpc_name" {
+    description = "The name of the VPC."
+    type        = string
+    default     = "Bastion Server VPC"
+}
+variable "vpc_cidr" {
+    description = "The CIDR block for the VPC."
+    type        = string
+    default     = "10.0.0.0/16"
+}
+variable "enable_nat_gateway" {
+    description = "Enable NAT Gateway for private subnets."
+    type        = bool
+    default     = true
+}
+
+// Bastion Host
+variable "bastion_instance_type" {
+    description = "The instance type for the bastion host."
+    type        = string
+    default     = "t3.micro"
+}
