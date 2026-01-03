e73d54bb1cbbb05f49c86957701b696ff57b0c8a
Yusuf <yusufrehmanit@gmail.com>
2026-01-02 23:11:29 -0800

    declared bastion and private instance  output values

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "bastion_public_ip" {
+    description = "The public IP address of the Bastion Host."
+    value       = aws_instance.bastion_host.public_ip
+}
+
+output "app_server_private_ip" {
+    description = "The private IP address of the Application Server."
+    value       = aws_instance.app_server.private_ip
+}
